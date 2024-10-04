d0e9f59812dd9e0215d4171912681fbf98e45953
jasjot190 <jasjot.singh@edu.sait.ca>
2024-10-03 21:27:40 -0600

    Fianl version of assignment week 5

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -10,17 +10,12 @@ export default function Form() {
   const handleNameChange = (e) => {
     let name = e.target.value;
     name = name.replace(/[^a-zA-Z\s]/g, "");
-    console.log(name.length);
-    err ? (name.length > 0 ? setErr(!err) & setName(name) : "") : setName(name);
+
+    name.length > 0 ? setErr(false) & setName(name) : setName(name);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const item = {
-      name: name,
-      quantity: quantity,
-      category: category,
-    };
     console.log(
       `Added item: ${name}, quantity: ${quantity}, category: ${category}`
     );
@@ -29,38 +24,34 @@ export default function Form() {
     setName("");
     setQuantity(1);
     setCategory("Produce");
-    err ? setErr(!err) : setErr(err);
   };
 
   return (
     <form
       action="submit"
       className="bg-slate-900 p-4 m-4"
-      onSubmit={(e) => handleSubmit(e)}
+      onSubmit={
+        name.length == 0
+          ? (e) => e.preventDefault() & setErr(true)
+          : (e) => handleSubmit(e)
+      }
     >
       <input
         type="text"
         placeholder="Item name"
         value={name}
+        // required
         onChange={(e) => handleNameChange(e)}
         className="w-full mt-1 border-2 border-gray-300 p-3 rounded-lg text-gray-500 font-semibold"
       />
-      <p className="text-red-700 mt-1" hidden={!err}>
-        *Name cannot be empty
-      </p>
+      {err && <p className="text-red-700 mt-1">*Name cannot be empty</p>}
       <div className="my-4 flex gap-10">
         <div className=" flex items-center w-fit bg-white  p-3 gap-4 rounded-lg">
           <div className="bg-black py-1 min-w-14  text-center">{quantity}</div>
           <div className="space-x-2">
             <button
               type="button"
-              disabled={(() => {
-                if (quantity != 1) {
-                  return false;
-                } else {
-                  return true;
-                }
-              })()}
+              disabled={quantity <= 1 ? true : false}
               className="bg-blue-500 hover:bg-blue-700 px-3 rounded-md disabled:bg-gray-400"
               onClick={() => setQuantity((prev) => prev - 1)}
             >
@@ -68,13 +59,7 @@ export default function Form() {
             </button>
             <button
               type="button"
-              disabled={(() => {
-                if (quantity != 20) {
-                  return false;
-                } else {
-                  return true;
-                }
-              })()}
+              disabled={quantity >= 20 ? true : false}
               className="bg-blue-500 hover:bg-blue-700 px-3 rounded-md disabled:bg-gray-400"
               onClick={() => setQuantity((prev) => prev + 1)}
             >
@@ -107,15 +92,8 @@ export default function Form() {
         </select>
       </div>
       <button
-        type="button"
+        type="submit"
         className="w-full bg-blue-500 rounded-lg p-3 font-bold text-lg hover:bg-blue-700"
-        onClick={
-          name.length > 0
-            ? (e) => handleSubmit(e)
-            : err
-            ? () => ""
-            : () => setErr(!err)
-        }
       >
         +
       </button>
