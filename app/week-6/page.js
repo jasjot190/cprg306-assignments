2c79e5e03c3fb1ce498ff47eaf288c0a0d849765
jasjot190 <jasjot.singh@edu.sait.ca>
2024-10-11 15:37:49 -0600

    Week 6 assignment completed along with optional section

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,5 +1,10 @@
-import React from "react";
-
+"use client";
+import { itemList } from "./item-list";
 export default function page() {
-  return <div>Week6 -- under construction</div>;
+  return (
+    <main className="bg-slate-950 p-4">
+      <h1 className="m-2 font-bold text-3xl">Shopping List</h1>
+      {itemList()}
+    </main>
+  );
 }
